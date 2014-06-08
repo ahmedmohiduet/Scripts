9b85801858ce3c7a09939c2c70b247ee71c4ece2
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2014-06-08 16:59:44 +0600

    Update maccordova.sh

--- maccordova.sh
+++ maccordova.sh
@@ -1,11 +1,30 @@
+#make sure ruby is installed
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+#need nodejs npm utility
 brew install nodejs
+#install cordova
 npm install -g cordova
 cd /usr/local/
+#create hello world project
 cordova create hello com.example.hello HelloWorld
 cd hello
 cordova platform add ios
 cordova platforms ls//see something like this .. ios 3.5.0
+#install iphone emulator
 npm install -g ios-sim
+#build project
 cordova build
+#run helloworld app
 cordova emulate ios
+#now, let's distribute our app (create .ipa distribution file)
+cd /platforms/ios/
+#install xcodeproj
+sudo gem install xcodeproj
+#download ruby script to generate project scheme
+wget --no-check-certificate -U Mozilla/5.0 https://raw.githubusercontent.com/ahmedmohiduet/Scripts/master/genscheme.rb
+#create project scheme
+ruby genscheme.rb
+#create xcarchive
+xcodebuild -scheme HelloWorld archive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -archivePath $(pwd)/HelloWorld.xcarchive
+#create ipa(unsigned)
+xcodebuild -project HelloWorld.xcodeproj -exportArchive -exportFormat ipa -archivePath $(pwd)/HelloWorld.xcarchive -exportPath $(pwd)/HelloWorld.ipa CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -alltargets -configuration Release
