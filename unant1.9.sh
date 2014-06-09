96436fcf09430fce9b10740e34a59118f3518149
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2014-06-10 02:20:47 +0600

    Update unant1.9.sh

--- unant1.9.sh
+++ unant1.9.sh
@@ -1,7 +1,13 @@
+#uninstall if already old versions are there
+yum remove ant
 cd /opt/
+#download ant
 wget http://mirrors.ispros.com.bd/apache//ant/binaries/apache-ant-1.9.4-bin.zip
+#unzip and delete zip(your choice)
 unzip apache-ant-1.9.4-bin.zip && rm apache-ant-1.9.4-bin.zip
 cd apache-ant-1.9.4/
+#create symlinks
 ln -s /opt/apache-ant-1.9.4 /opt/ant
 ln -s /opt/ant/bin/ant /usr/bin/ant
+#add environment variable
 echo 'ANT_HOME=/opt/ant' >> /etc/environment
