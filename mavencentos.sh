168a7434d80a654abd078d494f14b709bc6e7ce1
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2014-06-22 19:54:39 +0600

    Update mavencentos.sh

--- mavencentos.sh
+++ mavencentos.sh
@@ -1,6 +1,6 @@
 wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
-rm -f apache-maven-3.0.5-bin.tar.gz
 tar xzf apache-maven-3.0.5-bin.tar.gz -C /usr/local/
+rm -f apache-maven-3.0.5-bin.tar.gz
 cd /usr/local
 ln -s apache-maven-3.0.5 maven
 export M2_HOME=/usr/local/maven
