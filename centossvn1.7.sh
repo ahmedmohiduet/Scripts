7c08d1b1aaa28b1902c270836577b433de27e008
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2014-06-23 11:56:46 +0600

    Create centossvn1.7.sh

---
+++ centossvn1.7.sh
@@ -0,0 +1,5 @@
+yum remove svn
+cd /usr/local/
+wget http://opensource.wandisco.com/centos/5/devel/RPMS/x86_64/subversion-1.7.9-1.x86_64.rpm
+rpm -ivh subversion-1.7.9-1.x86_64.rpm
+rm -f subversion-1.7.9-1.x86_64.rpm
