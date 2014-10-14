edb1442d489bff7bb9da0607e9608e2cafd66b58
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2014-10-14 16:36:31 +0600

    Create installpycharmeap.sh

---
+++ installpycharmeap.sh
@@ -0,0 +1,7 @@
+cd /usr/local/
+wget http://download.jetbrains.com/python/pycharm-professional-138.2401.tar.gz
+tar -xzvf pycharm-professional-138.2401.tar.gz
+rm pycharm-professional-138.2401.tar.gz
+mv pycharm-138.2401/ pycharm/
+cd pycharm/bin/
+sh pycharm.sh &
