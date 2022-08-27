ddc9811c5cada4709e6e63215cab40ed311afea5
MD. Mohiuddin ahmed <ahmed.mohi.duet@gmail.com>
2022-08-27 16:39:11 +0600

    Update nextwin.sh
    fixed a bug which made navigation anomalous.

--- nextwin.sh
+++ nextwin.sh
@@ -4,11 +4,9 @@ pcName=`hostname`
 
 echo "Current window Id: $curWinId"
 winIds=`wmctrl -lp|grep -v " $pcName Desktop"|grep " 0 "|cut -d' ' -f 1`
+winCount=`wmctrl -lp|grep -v " $pcName Desktop"|grep " 0 "|cut -d' ' -f 1|wc -l`
 
 echo "Window list: $winIds"
-winCount=`echo $winIds|tr -cd ' \t' | wc -c`
-
-((winCount=winCount+1))
 
 echo "Window count: $winCount"
 readarray -t <<<$winIds
